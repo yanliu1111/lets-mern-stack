78b5b3b3103a49c994277523bf83a605c067b071
yanliu1111 <liuyan0504@gmail.com>
2025-08-25 19:52:24 -0600

    feat: build api by redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,20 @@ import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { store } from '../app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
