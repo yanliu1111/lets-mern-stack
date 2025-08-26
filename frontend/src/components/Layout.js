cf69f841517ba7726fe2c90866c2e59bd5a563a8
yanliu1111 <liuyan0504@gmail.com>
2025-08-25 18:56:03 -0600

    fix: layout edit

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,16 +1,6 @@
-import DashFooter from './DashFooter'
-import DashHeader from './DashHeader'
 import { Outlet } from 'react-router-dom'
 
-const DashLayout = () => {
-    return (
-        <>
-            <DashHeader />
-            <div className="dash-container">
-                <Outlet />
-            </div>
-            <DashFooter />
-        </>
-    )
+const Layout = () => {
+    return <Outlet />
 }
-export default DashLayout
+export default Layout
