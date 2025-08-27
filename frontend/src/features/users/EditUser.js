0fa63b6e7bb67f4b8d3b40195bc0ce95e94051f3
yanliu1111 <liuyan0504@gmail.com>
2025-08-26 19:51:07 -0600

    feat: build editNoteForm

--- frontend/src/features/users/EditUser.js
+++ frontend/src/features/users/EditUser.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import EditUserForm from './EditUserForm'
+import { selectUserById } from './usersApiSlice'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const EditUser = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const { id } = useParams()
 
+    const user = useSelector(state => selectUserById(state, id))
+
+    const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>
+
+    return content
+}
 export default EditUser
