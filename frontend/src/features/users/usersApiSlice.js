063af607502fac54f1c23c09f76103abcbcd915b
yanliu1111 <liuyan0504@gmail.com>
2025-08-26 18:05:24 -0600

    feat: RTk Query for NewUserForm

--- frontend/src/features/users/usersApiSlice.js
+++ frontend/src/features/users/usersApiSlice.js
@@ -33,11 +33,48 @@ export const usersApiSlice = apiSlice.injectEndpoints({ //inject endpoints to ex
               } else return [{ type: 'User', id: 'LIST' }]
           }
       }),
-  }),
-})
+      addNewUser: builder.mutation({
+        query: initialUserData => ({
+            url: '/users',
+            method: 'POST',
+            body: {
+                ...initialUserData,
+            }
+        }),
+        invalidatesTags: [
+            { type: 'User', id: "LIST" }
+        ]
+      }),
+      updateUser: builder.mutation({
+          query: initialUserData => ({
+              url: '/users',
+              method: 'PATCH',
+              body: {
+                  ...initialUserData,
+              }
+          }),
+          invalidatesTags: (result, error, arg) => [
+              { type: 'User', id: arg.id }
+          ]
+      }),
+      deleteUser: builder.mutation({
+          query: ({ id }) => ({
+              url: `/users`,
+              method: 'DELETE',
+              body: { id }
+          }),
+          invalidatesTags: (result, error, arg) => [
+              { type: 'User', id: arg.id }
+          ]
+      }),
+    }),
+  })
 
 export const {
   useGetUsersQuery,
+  useAddNewUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation
 } = usersApiSlice
 
 // returns the query result object
