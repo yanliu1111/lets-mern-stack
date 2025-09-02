25260ebf6b20a46511a3475ab239e532f01ed1b8
yanliu1111 <liuyan0504@gmail.com>
2025-09-02 15:34:39 -0600

    fix: permission limit

--- frontend/src/features/auth/Welcome.js
+++ frontend/src/features/auth/Welcome.js
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
 
 const Welcome = () => {
-    const {username, isManager, isAdmin} = {username: 'User', isManager: true, isAdmin: false} //useAuth()
+    const { username, isManager, isAdmin } = useAuth()
     const date = new Date()
     const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
 
