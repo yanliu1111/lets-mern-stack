33c96ffd8c1a498c5b21d3bd2049ebb26e2e907d
yanliu1111 <liuyan0504@gmail.com>
2025-09-02 19:50:03 -0600

    feat: update for requireAuth for route permission

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import NotesList from './features/notes/NotesList'
 import PersistLogin from './features/auth/PersistLogin'
 import Prefetch from './features/auth/Prefetch'
 import Public from './components/Public'
+import { ROLES } from './config/roles'
+import RequireAuth from './features/auth/RequireAuth'
 import UsersList from './features/users/UsersList'
 import Welcome from './features/auth/Welcome'
 
@@ -18,29 +20,37 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+      {/* public routes */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+
+      {/* protected routes */}
         <Route element={<PersistLogin />}>
-          <Route element={<Prefetch />}>
-            <Route path="dash" element={<DashLayout />}>
+          {/* RequireAuth is for protecting the routes based on user roles, in case regular user tries to access admin routes using /users */}
+          <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}>
+            <Route element={<Prefetch />}>
+              <Route path="dash" element={<DashLayout />}>
 
-              <Route index element={<Welcome />} />
+                <Route index element={<Welcome />} />
 
-              <Route path="users">
-                  <Route index element={<UsersList />} />
-                  <Route path=":id" element={<EditUser />} />
-                  <Route path="new" element={<NewUserForm />} />
+                <Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />}>
+                  <Route path="users">
+                      <Route index element={<UsersList />} />
+                      <Route path=":id" element={<EditUser />} />
+                      <Route path="new" element={<NewUserForm />} />
+                    </Route>
                 </Route>
-
+                
                 <Route path="notes">
                   <Route index element={<NotesList />} />
                   <Route path=":id" element={<EditNote />} />
                   <Route path="new" element={<NewNote />} />
-              </Route>
+                </Route>
 
-            </Route>{/* End Dash */}
-          </Route>{/* End Prefetch */}
-        </Route>
+              </Route>{/* End Dash */}
+            </Route>{/* End Prefetch */}
+          </Route> {/* End of RequireAuth */}
+        </Route> {/* End of PertectRoutes */}
 
       </Route>
     </Routes>
